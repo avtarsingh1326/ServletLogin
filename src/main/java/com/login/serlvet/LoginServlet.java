b62939b1046542f3d1de9affde7c3ce41ae4bc59
asingh0711 <139422827+asingh0711@users.noreply.github.com>
2023-09-23 11:20:14 -0400

    first commit

---
+++ src/main/java/com/login/serlvet/LoginServlet.java
@@ -0,0 +1,55 @@
+package com.login.serlvet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String loginUser = "user123";
+		String loginPassword = "abc123";
+		
+		String username = request.getParameter("username");
+ 		String password = request.getParameter("password");
+		
+ 		PrintWriter out =  response.getWriter();
+ 		
+ 		if(username.equals(loginUser) && password.equals(loginPassword)) {
+ 			out.print("Welcome " + username + " to my application.");
+ 		}else {
+ 			out.print("Username or password is not correct.");
+ 		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
